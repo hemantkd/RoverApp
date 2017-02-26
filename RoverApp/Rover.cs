c933a50b618f239db96002b4cb426555ddd5d6a5
Hemant Kumar Dundigalla <d.hemant.kr@gmail.com>
2017-02-26 23:16:11 +0000

    initial commit

---
+++ RoverApp/Rover.cs
@@ -0,0 +1,9 @@
+﻿namespace RoverApp
+{
+    public class Rover
+    {
+	    public int XCoordinate { get; set; }
+	    public int YCoordinate { get; set; }
+	    public char DirectionFacing { get; set; }
+    }
+}
